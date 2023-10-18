b66de31bb09362bf73a4cf105bb0a673c123c652
GabrielZuany <gzuanydev@gmail.com>
2023-10-18 15:24:58 -0300

    Create utils.c

---
+++ Source/libs/src/utils.c
@@ -0,0 +1,11 @@
+#include "../libs/utils.h"
+#include "../libs/graph.h"
+#include "../libs/heap.h"
+#include "../libs/item.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+void dijkstra(void** args){
+    return;
+}
