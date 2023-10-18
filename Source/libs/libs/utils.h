6acd3b96507deed8d918f6e4688546ee975e64d7
GabrielZuany <gzuanydev@gmail.com>
2023-10-18 15:25:06 -0300

    Create utils.h

---
+++ Source/libs/libs/utils.h
@@ -0,0 +1,13 @@
+#ifndef UTILS_H
+#define UTILS_H
+
+#include "graph.h"
+#include "heap.h"
+#include "item.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+void dijkstra(void** args);
+
+#endif
