33c50edad51bd249f9f7d0a561ed778cc76fa4dd
GabrielZuany <gzuanydev@gmail.com>
2023-10-18 15:25:09 -0300

    Create rtt.h

---
+++ Source/libs/libs/rtt.h
@@ -0,0 +1,14 @@
+#ifndef RTT_H
+#define RTT_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include "../libs/graph.h"
+#include "../libs/utils.h"
+#include "../libs/heap.h"
+#include "../libs/item.h"
+
+
+
+
+#endif
