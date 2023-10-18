e044f802528fcdd896fc3aba96fa448d25b280aa
GabrielZuany <gzuanydev@gmail.com>
2023-10-18 15:28:20 -0300

    Move pseudo code from rtt.c to utils.c

--- Source/libs/src/rtt.c
+++ Source/libs/src/rtt.c
@@ -1,16 +1,3 @@
-//  D ← [∞,∞, . . . , ∞] /* n copies of ∞ */
-//  D[s] ← 0
-//  P ← [ ]
-//  Q ← V /* list of nodes to visit */
-//  while length(Q) > 0 do
-//     find v ∈ Q such that D[v] is minimal
-//     Q ← remove(Q, v)
-//     for each u ∈ adj(v) ∩ Q do
-//         if D[u] > D[v] + w(vu) then
-//            D[u] ← D[v] + w(vu)
-//            P[u] ← v
-//  return (D, P)
-
 #include <stdio.h>
 #include <stdlib.h>
 #include "../libs/rtt.h"
--- Source/libs/src/utils.c
+++ Source/libs/src/utils.c
@@ -6,6 +6,20 @@
 #include <stdlib.h>
 #include <string.h>
 
+
+
 void dijkstra(void** args){
+    //  D ← [∞,∞, . . . , ∞] /* n copies of ∞ */
+    //  D[s] ← 0
+    //  P ← [ ]
+    //  Q ← V /* list of nodes to visit */
+    //  while length(Q) > 0 do
+    //     find v ∈ Q such that D[v] is minimal
+    //     Q ← remove(Q, v)
+    //     for each u ∈ adj(v) ∩ Q do
+    //         if D[u] > D[v] + w(vu) then
+    //            D[u] ← D[v] + w(vu)
+    //            P[u] ← v
+    //  return (D, P)
     return;
 }
